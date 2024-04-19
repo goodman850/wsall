e5e753bd30482a1c516d565991e64f90e4e99ff6
goodman850 <virtualdesign@yahoo.com>
2024-04-20 03:25:57 +0330

    final

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -23,7 +23,7 @@ systemctl enable chrony && systemctl restart chrony
 timedatectl set-timezone Asia/Tehran
 chronyc sourcestats -v
 chronyc tracking -v
-latest_version="1.8.4"
+latest_version="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
 # / / Installation Xray Core
 xraycore_link="https://github.com/XTLS/Xray-core/releases/download/v$latest_version/xray-linux-64.zip"
 
